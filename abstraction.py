85d70a8d6dd2f1a1a94d01131cefc0cbb7d38a64
simicr <simradovan@gmail.com>
2024-11-12 19:14:07 +0100

    a1 final

--- abstraction.py
+++ abstraction.py
@@ -9,13 +9,13 @@ def edge_detection(im):
     '''Implement DoG smooth edge detection (Eq. 6)'''
 
     se = skf.gaussian(im, sigma=sigma_e) 
-    sf = skf.gaussian(im, sigma=np.sqrt(1.6) * sigma_e) # Po formuli dao.
+    sf = skf.gaussian(im, sigma=np.sqrt(1.6) * sigma_e) # given formula
 
-    diff = se - tau * sf  # Oduzmmeo
-    mask = diff < 0 # Nadjemo sve manje od 0
+    diff = se - tau * sf  # substract
+    mask = diff < 0 # find all that are smaller then 0
     
     dog = np.ones_like(im) 
-    dog[mask] = 1 + np.tanh(phi_e * diff[mask]) # Sve manje od 0 provucemo kroz formulu, ostalo ostaje 1.
+    dog[mask] = 1 + np.tanh(phi_e * diff[mask]) # apply formula to all that are smaller then 0, rest stays 1
 
     return dog
   
@@ -23,15 +23,15 @@ def edge_detection(im):
 def luminance_quantization(im):
     '''Implement luminance quantization (Eq. 8)'''
     
-    lmax = 100.0 # Zasto je napisao u opisu zadatka da je 100?
-    deltaq = lmax / n_bins # Izracunam razmak koji ce biti
-    q = np.linspace(0, lmax, n_bins) # Dobijem niz svih razmaka
+    lmax = 100.0 
+    deltaq = lmax / n_bins # calculate step
+    q = np.linspace(0, lmax, n_bins) # get an array of all steps
 
-    diff = np.abs((im[..., None] - q.reshape(1, -1))) # Od svakog piksela izrazunamo razmak od dva granicna broja (i, i+deltaq) 
-    argmin = np.argmin(diff, axis=-1) # Nadjemo pozicije najblizih
-    closest_qs = q[argmin] # Nadjemo najblize
+    diff = np.abs((im[..., None] - q.reshape(1, -1))) # difference between (i, i+deltaq) for every step
+    argmin = np.argmin(diff, axis=-1) # find positions of the closest ones
+    closest_qs = q[argmin] # find the closest ones
 
-    ss_phiq = closest_qs + (deltaq/2) * np.tanh(phi_q*(im - closest_qs)) # Data formula
+    ss_phiq = closest_qs + (deltaq/2) * np.tanh(phi_q*(im - closest_qs)) # given formula
     return ss_phiq
 
 
@@ -44,26 +44,23 @@ def bilateral_gaussian(im):
     Apply it to the padded image.
     '''
 
-
-    windows = ns.sliding_window_view(padded , (2*r+1, 2*r+1, 3)) # Napravice array (400, 254, 1, 17,17,3), u sustini prva dva broja su mesto piksela, ostatak je sam prozor. 
-    chnanel_diff = windows - im[:, :, None, None, None, :] # Oduzmem od svakog prozora centralni piksel, ovo bi trebalo da radi. 
-    luminance_magnitude = np.sqrt(np.sum((chnanel_diff ** 2), axis=-1)) # Izracunam L2 normu te razlike. 
-    luminance_magnitude = np.exp(-luminance_magnitude**2 / (2.0*sigma_r**2)) # Ukoliko ovo prethodno radi imacemo (400, 254, 1, 17, 17). tj onaj deo |F(p) - F(q)|_f u zadatku. 
+    windows = ns.sliding_window_view(padded , (2*r+1, 2*r+1, 3)) # makes an array (400, 254, 1, 17,17,3), first two numbers are for pixel position, the rest is the window itself. 
+    chnanel_diff = windows - im[:, :, None, None, None, :] # substract central pixel intensity from every window 
+    intensity_magnitude = np.sqrt(np.sum((chnanel_diff ** 2), axis=-1)) # calculate L2 norm of difference 
+    intensity_magnitude = np.exp(-intensity_magnitude**2 / (2.0*sigma_r**2)) # results in (400, 254, 1, 17, 17); |F(p) - F(q)|_f. 
 
     indices = np.indices((2*r+1, 2*r+1)) + 1  
     pixels = np.stack(indices, axis=-1) 
-    pixels = pixels - pixels[r,r,:] # Meni nesto deluje da svaki prozor kada je razlika piksela u pitanju je uvek isti prozor. I onda mi je ideja da ovo izracunamo samo jednom 
-                                    # i posle da ne moramo uopste. 
+    pixels = pixels - pixels[r,r,:] # for pixel difference every window is the same, therefore we calculate it only once.
 
-    pixels_magnitude = np.linalg.norm(pixels, axis=-1, ord=2, keepdims=False) # Izracunam L2 normu, nisam siguran za ovaj keep dims, ali dobije se taman (17,17) sto mozemo samo 
-                                                                                   # da pomnozimo sa luminance magnitude.
-    pixels_magnitude = np.exp(-pixels_magnitude**2/ (2.0*sigma_s**2)) # Po formuli opet fali /2
+    pixels_magnitude = np.linalg.norm(pixels, axis=-1, ord=2, keepdims=False) # calculate L2 norm
+    pixels_magnitude = np.exp(-pixels_magnitude**2/ (2.0*sigma_s**2)) 
 
-    weights =  pixels_magnitude * luminance_magnitude # Pomnozimo ih ovo bi trebalo da bude b(|p-q|_inf)b(|F(p) - F(q)|_f) za svaki prozor. 
-    sum_weights = np.sum(weights, axis=(-1,-2, -3)) # Sumiramo, to je delioc u onoj formuli. 
+    weights =  pixels_magnitude * intensity_magnitude # b(|p-q|_inf)b(|F(p) - F(q)|_f) for every window. 
+    sum_weights = np.sum(weights, axis=(-1,-2, -3)) # detominator. 
     
     filtered = weights[..., None] * windows # b(|p-q|_inf)b(|F(p) - F(q)|_f) * F(q)
-    filtered = np.sum(filtered, axis=(-2, -3, -4)) / sum_weights[..., None] # Sumiramo sve prozore, i izbacimo onu jedinicu u (400, 254, 1 ...)
+    filtered = np.sum(filtered, axis=(-2, -3, -4)) / sum_weights[..., None] # summ all windows
 
     return filtered 
 
@@ -72,22 +69,17 @@ def abstraction(im):
     filtered = skc.rgb2lab(im)
     for i in range(n_e):
         filtered = bilateral_gaussian(filtered)
-        imageio.imsave(f'bilateral-it{i}.png', skc.lab2rgb(filtered))
 
     edges = edge_detection(filtered[:, :, 0])
-    imageio.imsave('edges.png', edges)
  
     for i in range(n_b - n_e):
         filtered = bilateral_gaussian(filtered)
-        imageio.imsave(f'bilateral-it{ n_e + i}.png', skc.lab2rgb(filtered))
 
     luminance_quantized = luminance_quantization(filtered[:, :, 0])
-    imageio.imsave('lumquant.png', luminance_quantized)
-
-    filtered[:, :, 0] = luminance_quantized * edges # Valjda je ovo dobro
 
     '''Get the final image by merging the channels properly'''
-    combined = np.clip(filtered, [0, -128, -128], [100, 127, 127])  # Todo
+    filtered[:, :, 0] = luminance_quantized * edges 
+    combined = np.clip(filtered, [0., -128., -128.], [100., 127., 127.]) 
     return skc.lab2rgb(combined)
 
 
